de12060e1643a8cb600c747018638f98aabd61b4
Contador Espíndola <vce@hotmail.com.ar>
2022-02-14 14:41:11 -0300

    Detalle y Registro terminado

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+
+app.listen(2500, ()=>{
+    console.log('Servidor funcionando');
+});
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/index.html');
+});
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/carrito', (req,res)=>{
+    res.sendFile(__dirname + '/views/productCart.html');
+});
+
+app.get('/detalle', (req,res)=>{
+    res.sendFile(__dirname + '/views/productDetail.html');
+});
+
+
