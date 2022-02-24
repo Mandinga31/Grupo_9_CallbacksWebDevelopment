1730cf2d856f739d3890ef049d27f0e21e4305dc
Contador Espíndola <vce@hotmail.com.ar>
2022-02-23 22:34:16 -0300

    Agregado sistema de rutas y ejs

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
+const mainRoutes = require('./routes/mainroutes')
 app.use(express.static('public'));
-
+app.set('view engine', 'ejs')
+app.use(mainRoutes);
 
 app.listen(2500, ()=>{
     console.log('Servidor funcionando');
---
+++ controllers/maincontrollers.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const path = require('path')
+const controlador = {
+
+index: (req,res)=>{
+res.render(path.join(__dirname, '../views/index.ejs'))
+},
+register: (req,res)=>{
+    res.render(path.join(__dirname, '../views/register.ejs'));
+},
+login: (req,res)=>{
+    res.render(path.join(__dirname,  '../views/login.ejs'));
+},
+carrito: (req,res)=>{
+    res.render(path.join(__dirname,  '../views/productCart.ejs'));
+},
+detalle: (req,res)=>{
+    res.render(path.join(__dirname , '../views/productDetail.ejs'));
+}
+};
+module.exports = controlador;
