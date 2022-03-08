e5bd457d0c6c3e2aff319f955650d61d291afa72
Contador Espíndola <vce@hotmail.com.ar>
2022-03-07 22:26:52 -0300

    Solucionado margin de carrito y create

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
-const mainRoutes = require('./routes/mainroutes')
+const mainRoutes = require('./routes/mainroutes');
+const methodOverride = require('method-override');
 app.use(express.static('public'));
 app.set('view engine', 'ejs')
 app.use(mainRoutes);
+app.use(methodOverride('_method'))
+
 
 app.listen(2500, ()=>{
     console.log('Servidor funcionando');
