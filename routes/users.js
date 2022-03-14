6f33409e351bea4d2ee9540a1cfa8f9e74436b08
Contador Espíndola <vce@hotmail.com.ar>
2022-03-14 19:47:17 -0300

    nueva orginazion, create terminado

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,20 @@
 
+
 // ************ Require's ************
 const express = require('express');
 const router = express.Router();
 
 
 // ************ Controller Require ************
-const mainController = require('../controllers/maincontrollers');
+const userController = require('../controllers/usercontroller');
 
-router.get('/', mainController.index); 
+router.get('/register', userController.register);
+
+router.get('/login', userController.login); 
 
 module.exports = router;
+
+
+
+
+
